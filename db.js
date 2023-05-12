b016b175aee69b3441abca0cce5a29bd86dda9a2
Bisma Naeem baig <bismanaeembaig@gmail.com>
2023-05-12 13:53:37 +0500

    Update db.js

--- db.js
+++ db.js
@@ -1,4 +1,4 @@
 var mongoose = require('mongoose');
-var uri = "mongodb+srv://pilsa:pilsa@plisa-fuc3q.mongodb.net/pilsa_db?retryWrites=true";
+var uri = "mongodb+srv://bismanaeembaig:nOXV7dI8EL39DxY8@cluster0.p5pxm7a.mongodb.net/?retryWrites=true&w=majority";
 
 mongoose.connect(uri, { useNewUrlParser: true });
